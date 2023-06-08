7def0dceaddbc1cf3946c3ccc4997f85b738ccef
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-08 15:04:45 +0200

    Add files via upload

---
+++ iso-creator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+udevil mount og-debian.iso
+wait
+cp -rT /media/root/og-debian.iso isofiles/
+wait
+
+chmod +w -R isofiles/install.amd/
+gunzip isofiles/install.amd/initrd.gz
+echo preseed.cfg | cpio -H newc -o -A -F isofiles/install.amd/initrd
+gzip isofiles/install.amd/initrd
+chmod -w -R isofiles/install.amd/
+
+cd isofiles
+chmod +w md5sum.txt
+find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
+chmod -w md5sum.txt
+cd ..
+
+genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat \
+            -no-emul-boot -boot-load-size 4 -boot-info-table \
+            -o preseed-sidx.iso isofiles
+
+rm -r isofiles/
