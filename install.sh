2c70b7ec2e2450592dc2134766854f255d3e4ec3
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-12 22:50:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ cat <<EOT >> /etc/apt/sources.list
 deb http://deb.debian.org/debian testing main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian testing main contrib non-free non-free-firmware
 
-deb http://security.debian.org/debian-security testing main contrib non-free non-free-firmware
-deb-src http://security.debian.org/debian-security testing main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
 
 deb http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
