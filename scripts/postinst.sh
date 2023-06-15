32d2f5d1681e985145dde9305bc7d636e22b5beb
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-15 11:53:22 +0200

    Update postinst.sh

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub org.libreoffice.LibreOffice -y
@@ -17,6 +17,37 @@ lspci_output_amd=$(lspci)
 if echo "$lspci_output_amd" | grep -i "AMD" | grep -i "VGA" >/dev/null; then
   apt install libdrm-amdgpu1 xserver-xorg-video-amdgpu mesa-vulkan-drivers libvulkan1 vulkan-tools vulkan-validationlayers
 fi
+
+echo "Recreating sources list"
+
+rm /etc/apt/sources.list
+touch /etc/apt/sources.list
+
+cat <<EOT >> /etc/apt/sources.list
+deb http://deb.debian.org/debian testing main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian testing main contrib non-free non-free-firmware
+
+deb http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security testing-security main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian testing-updates main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian sid main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian sid main contrib non-free non-free-firmware
+EOT
+
+echo "Setting repository priority"
+cat <<EOT >> /etc/apt/preferences.d/default
+package: *
+Pin: release a=sid
+Pin-Priority: 100
+
+package: *
+Pin: release a=testing
+Pin-Priority: 90
+EOT
+
 
 echo "Upgrading system and removing unnecessary packages"
 sleep 2
