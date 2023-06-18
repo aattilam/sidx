bb0c22636b905659dd366a032d8ad5c76d8f6441
AttilaM <59540584+aattilam@users.noreply.github.com>
2023-06-18 12:53:02 +0200

    Update iso-creator.sh

--- iso/iso-creator.sh
+++ iso/iso-creator.sh
@@ -5,21 +5,20 @@ wait
 cp -rT /media/og-debian.iso isofiles/
 wait
 
-chmod +w -R isofiles/install.amd/
+chmod +w -R isofiles/
 gunzip isofiles/install.amd/initrd.gz
 echo preseed.cfg | cpio -H newc -o -A -F isofiles/install.amd/initrd
 gzip isofiles/install.amd/initrd
-chmod -w -R isofiles/install.amd/
 
 cd isofiles
-chmod +w md5sum.txt
 find -follow -type f ! -name md5sum.txt -print0 | xargs -0 md5sum > md5sum.txt
-chmod -w md5sum.txt
 cd ..
+chmod -w -R isofiles/
 
 genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat \
             -no-emul-boot -boot-load-size 4 -boot-info-table \
             -o preseed-sidx.iso isofiles
 
+chmod +w -R isofiles/
 rm -r isofiles/
 udevil umount og-debian.iso
